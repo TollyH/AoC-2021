a6786efaee578ee699e6502fd17b0697ac03963a
TollyH <ptolemyhill@sky.com>
2021-12-07 18:41:44 +0000

    Completed Day 7

---
+++ D7/part_one.py
@@ -0,0 +1,14 @@
+with open("input.txt") as file:
+    puzzle_input = file.read().strip().split(",")
+
+positions = [int(x) for x in puzzle_input]
+targets = {}
+
+for target in range(max(positions)):
+    targets[target] = 0
+    for pos in positions:
+        targets[target] += abs(pos - target)
+
+optimal_target = min(targets, key=lambda x: targets[x])
+
+print(f"Position: {optimal_target}, Fuel: {targets[optimal_target]}")
---
+++ D7/part_two.py
@@ -0,0 +1,15 @@
+with open("input.txt") as file:
+    puzzle_input = file.read().strip().split(",")
+
+positions = [int(x) for x in puzzle_input]
+targets = {}
+
+for target in range(max(positions)):
+    targets[target] = 0
+    for pos in positions:
+        difference = abs(pos - target)
+        targets[target] += 0.5 * difference * (difference + 1)
+
+optimal_target = min(targets, key=lambda x: targets[x])
+
+print(f"Position: {optimal_target}, Fuel: {targets[optimal_target]}")
