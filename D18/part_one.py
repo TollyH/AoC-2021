379a980da9d6032a4e63427a8ad33f13695fccda
TollyH <ptolemyhill@sky.com>
2021-12-18 20:10:56 +0000

    Completed Day 18

---
+++ D18/part_one.py
@@ -0,0 +1,114 @@
+def parse_snailfish_number(string):
+    raw_num = string.removeprefix("[").removesuffix("]")
+    raw_contained = []
+    open_brackets = 0
+    last_insert = 0
+    for i, char in enumerate(raw_num):
+        if char == "[":
+            open_brackets += 1
+        elif char == "]":
+            open_brackets -= 1
+        elif char == "," and open_brackets == 0:
+            raw_contained.append(raw_num[last_insert:i])
+            last_insert = i + 1
+    raw_contained.append(raw_num[last_insert:])
+    contained = []
+    for item in raw_contained:
+        if item.isnumeric():
+            contained.append(int(item))
+        else:
+            contained.append(parse_snailfish_number(item))
+    return contained
+
+
+def get_all_indices(num, current):
+    indices = []
+    for index, item in enumerate(num):
+        if isinstance(item, list):
+            indices += get_all_indices(item, current + [index])
+        else:
+            indices.append(current + [index])
+    return indices
+
+
+def search_list_of_lists(source, target):
+    for index, item in enumerate(source):
+        if item == target:
+            return index
+    raise ValueError
+
+
+def explode(num, full_num, indices):
+    if len(indices) >= 4:
+        all_indices = get_all_indices(full_num, [])
+        first_num_index = search_list_of_lists(all_indices, indices + [0])
+        second_num_index = search_list_of_lists(all_indices, indices + [1])
+        if first_num_index >= 1:
+            prev_indices = all_indices[first_num_index - 1]
+            prev_item = full_num
+            for prev_index in prev_indices[:-1]:
+                prev_item = prev_item[prev_index]
+            prev_item[prev_indices[-1]] += num[0]
+        if second_num_index < len(all_indices) - 1:
+            next_indices = all_indices[second_num_index + 1]
+            next_item = full_num
+            for next_index in next_indices[:-1]:
+                next_item = next_item[next_index]
+            next_item[next_indices[-1]] += num[1]
+        full_item = full_num
+        for index in indices[:-1]:
+            full_item = full_item[index]
+        full_item[indices[-1]] = 0
+        return True
+    for index, item in enumerate(num):
+        if isinstance(item, list):
+            if explode(item, full_num, indices + [index]):
+                return True
+    return False
+
+
+def split(num):
+    for index, item in enumerate(num):
+        if isinstance(item, list):
+            if split(item):
+                return True
+        elif item >= 10:
+            num[index] = [item // 2, item // 2 + item % 2]
+            return True
+    return False
+
+
+def reduce_snailfish_number(num):
+    performed_operation = explode(num, num, [])
+    if not performed_operation:
+        performed_operation = split(num)
+
+    if not performed_operation:
+        return num
+    return reduce_snailfish_number(num)
+
+
+def calculate_magnitude(num):
+    if isinstance(num, int):
+        return num
+    magnitude_total = 0
+    for index, item in enumerate(num):
+        if index == 0:
+            magnitude_total += 3 * calculate_magnitude(item)
+        else:
+            magnitude_total += 2 * calculate_magnitude(item)
+    return magnitude_total
+
+
+with open("input.txt") as file:
+    puzzle_input = file.read().strip().splitlines()
+
+sf_numbers = []
+for sf_num in puzzle_input:
+    sf_numbers.append(parse_snailfish_number(sf_num))
+
+total = sf_numbers[0]
+for sf_num in sf_numbers[1:]:
+    total = reduce_snailfish_number([total, sf_num])
+
+print(calculate_magnitude(total))
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Advent of Code 2021
 
-My solutions written in Python - they are not necessarily the optimal solutions, nor are they necessarily particularly well written, as long as they give the correct answer, they're good enough for me. I am solving every day without any external help, and, for an additional challenge, attempting to do so without importing any libraries, not even ones in the Python Standard Library.
+My solutions written in Python - they are not necessarily the optimal solutions, nor are they necessarily particularly well written, as long as they give the correct answer, they're good enough for me. I am solving every day with as minimal help as possible, and, for an additional challenge, attempting to do so without importing any libraries, not even ones in the Python Standard Library.
 
 I am not including my puzzle input with the solutions, as they should work with anyone's (it looks for it in a file called `input.txt` in the same folder as the script).
