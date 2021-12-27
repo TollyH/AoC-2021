f3fdbd68356a4883a52b2f2cfcf3b2777c38bf88
TollyH <ptolemyhill@sky.com>
2021-12-27 22:33:20 +0000

    Visualised Day 25

--- D25/part_one.py
+++ D25/part_one.py
@@ -1,8 +1,36 @@
+import os
+
+from PIL import Image
+
 with open("input.txt") as file:
     puzzle_input = file.read().splitlines()
 
+if not os.path.isdir("output"):
+    os.mkdir("output")
+
+
+def save_cucumber_image(cucumber_grid, move_counter):
+    img = Image.new(
+        "RGB", (len(cucumber_grid[0]) * 8, len(cucumber_grid) * 8), "#bf6730"
+    )
+    for y, y_cucumbers in enumerate(cucumber_grid):
+        for x, cucumber_pixel in enumerate(y_cucumbers):
+            if cucumber_pixel != ".":
+                for ix in range(8):
+                    for iy in range(8):
+                        img.putpixel(
+                            ((x * 8) + ix, (y * 8) + iy),
+                            (207, 40, 31)
+                            if cucumber_pixel == ">"
+                            else (31, 90, 207)
+                        )
+    img.save(f"output/{move_counter:04d}.png")
+
+
 cucumbers = [list(x) for x in puzzle_input]
 new_cucumbers = None
+
+save_cucumber_image(cucumbers, 0)
 
 counter = 0
 performed_move = True
@@ -26,5 +54,10 @@ while performed_move:
                 performed_move = True
     cucumbers = [x.copy() for x in new_cucumbers]
     counter += 1
+    save_cucumber_image(cucumbers, counter)
+
+os.system(
+    "ffmpeg -f image2 -framerate 25 -i output/%04d.png output.gif"
+)
 
 print(counter)
