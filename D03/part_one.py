b0981714733b8619656762d8c930ddb0c28c40f9
TollyH <ptolemyhill@sky.com>
2021-12-27 20:26:35 +0000

    Visualised Day 03

--- D03/part_one.py
+++ D03/part_one.py
@@ -1,5 +1,19 @@
+import os
+
+from PIL import Image
+
 with open("input.txt") as file:
     puzzle_input = file.read().strip().splitlines()
+
+if not os.path.isdir("output_part_one"):
+    os.mkdir("output_part_one")
+
+img = Image.new('1', (len(puzzle_input[0]) * 42, len(puzzle_input)))
+for y, bin_str in enumerate(puzzle_input):
+    for x, digit in enumerate(bin_str):
+        for i in range(42):
+            img.putpixel(((x * 42) + i, y), int(digit))
+img.save("output_part_one/0000.png")
 
 gamma_rate_str = ''
 epsilon_rate_str = ''
@@ -8,6 +22,14 @@ for column in range(len(puzzle_input[0])):
     all_bits = [x[column] for x in puzzle_input]
     gamma_rate_str += max(('0', '1'), key=all_bits.count)
     epsilon_rate_str += min(('0', '1'), key=all_bits.count)
+    for x in range(42):
+        for y in range(len(puzzle_input)):
+            img.putpixel((x + (column * 42), y), int(gamma_rate_str[-1]))
+    img.save(f"output_part_one/{column + 1:04d}.png")
+
+os.system(
+    "ffmpeg -f image2 -framerate 1 -i output_part_one/%04d.png part_one.gif"
+)
 
 gamma_rate = int(gamma_rate_str, 2)
 epsilon_rate = int(epsilon_rate_str, 2)
--- D03/part_two.py
+++ D03/part_two.py
@@ -1,5 +1,19 @@
+import os
+
+from PIL import Image
+
 with open("input.txt") as file:
     puzzle_input = file.read().strip().splitlines()
+
+if not os.path.isdir("output_part_two"):
+    os.mkdir("output_part_two")
+
+img = Image.new('1', (len(puzzle_input[0]) * 42, len(puzzle_input)))
+for y, bin_str in enumerate(puzzle_input):
+    for x, digit in enumerate(bin_str):
+        for i in range(42):
+            img.putpixel(((x * 42) + i, y), int(digit))
+img.save("output_part_two/0000.png")
 
 oxygen_gen_rate_list = puzzle_input.copy()
 c02_scrub_rate_list = puzzle_input.copy()
@@ -26,6 +40,25 @@ for column in range(len(puzzle_input[0])):
         c02_scrub_rate_list = [
             x for x in c02_scrub_rate_list if x[column] == least_common
         ]
+    img = Image.new('1', (len(puzzle_input[0]) * 42, len(puzzle_input)), 1)
+    for y, bin_str in enumerate(oxygen_gen_rate_list):
+        for x, digit in enumerate(bin_str):
+            for i in range(42):
+                img.putpixel(
+                    (
+                        (x * 42) + i,
+                        (
+                            len(puzzle_input) // 2 -
+                            len(oxygen_gen_rate_list) // 2
+                        ) + y
+                    ),
+                    int(digit)
+                )
+    img.save(f"output_part_two/{column + 1:04d}.png")
+
+os.system(
+    "ffmpeg -f image2 -framerate 1 -i output_part_two/%04d.png part_two.gif"
+)
 
 oxygen_gen_rate = int(oxygen_gen_rate_list[0], 2)
 c02_scrub_rate = int(c02_scrub_rate_list[0], 2)
