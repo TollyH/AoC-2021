e7c074d6d8eff10537186864096c761b2965e9a7
TollyH <ptolemyhill@sky.com>
2021-12-27 13:37:18 +0000

    Visualised Day 02

--- D02/part_one.py
+++ D02/part_one.py
@@ -1,18 +1,25 @@
+import matplotlib.pyplot as plt
+
 with open("input.txt") as file:
     puzzle_input = file.read().strip().splitlines()
 
-horizontal = 0
-depth = 0
+horizontal = [0]
+depth = [0]
 
 for line in puzzle_input:
     split_line = line.split(" ")
     instruction = split_line[0]
     parameter = int(split_line[1])
     if instruction == "forward":
-        horizontal += parameter
+        horizontal.append(horizontal[-1] + parameter)
+        depth.append(depth[-1])
     elif instruction == "down":
-        depth += parameter
+        depth.append(depth[-1] + parameter)
+        horizontal.append(horizontal[-1])
     elif instruction == "up":
-        depth -= parameter
+        depth.append(depth[-1] - parameter)
+        horizontal.append(horizontal[-1])
 
-print(horizontal * depth)
+print(horizontal[-1] * depth[-1])
+plt.plot(horizontal, list(map(lambda x: -x, depth)))
+plt.savefig("part_one.png", dpi=200)
--- D02/part_two.py
+++ D02/part_two.py
@@ -1,8 +1,10 @@
+import matplotlib.pyplot as plt
+
 with open("input.txt") as file:
     puzzle_input = file.read().strip().splitlines()
 
-horizontal = 0
-depth = 0
+horizontal = [0]
+depth = [0]
 aim = 0
 
 for line in puzzle_input:
@@ -10,11 +12,13 @@ for line in puzzle_input:
     instruction = split_line[0]
     parameter = int(split_line[1])
     if instruction == "forward":
-        horizontal += parameter
-        depth += aim * parameter
+        horizontal.append(horizontal[-1] + parameter)
+        depth.append(depth[-1] + (aim * parameter))
     elif instruction == "down":
         aim += parameter
     elif instruction == "up":
         aim -= parameter
 
-print(horizontal * depth)
+print(horizontal[-1] * depth[-1])
+plt.plot(horizontal, list(map(lambda x: -x, depth)))
+plt.savefig("part_two.png", dpi=200)
